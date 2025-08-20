6a0d79d156f536415c071a8949b08064618a1d5c
Amal A M <vectorhero007@gmail.com>
2025-08-20 11:39:37 +0530

    useRef

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import './App.scss'
 
 
 function App() {
 
-  const [Number, stateName] = useState(0)
 
-  useEffect(() => {
-    console.log('page render');
-  }, [Number])
+  const testInput = useRef(null)
+
+  const [testValue, testState] = useState('')
+
+  const changePlaceHolder = () => {
+    testInput.current.placeholder = "youtube"
+    testInput.current.focus()
+    console.log(testInput);
+    
+    testState(testInput.current.value)
+  }
 
   return (
     <div className="App" style={{fontSize: 30}}>
-      <p>{Number} <br /> <button onClick={() => stateName(Number + 1)}>Increase number</button> </p>
+      
+      <input type="text" placeholder="whatsapp" ref={testInput} />
+      <button onClick={changePlaceHolder}>Change Placeholder</button>
+
+      <h1>{testValue}</h1>
+
     </div>
   );
 }
