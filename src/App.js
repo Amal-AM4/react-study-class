04c51f279f9166caad752a9b6f75369b412bbb2c
Amal A M <vectorhero007@gmail.com>
2025-08-25 09:31:59 +0530

    styled-components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import { useEffect, useRef, useState } from "react";
 import './App.scss'
-// import Task from "./components/Task";
+import { Button } from './components/Buttons';
+
 
 function App() {
 
@@ -8,17 +9,7 @@ function App() {
   return (
     <div className="App">
 
-      <form>
-        <label htmlFor="firstName">First Name: </label>
-        <input type="text" id="firstName" placeholder="First Name" /> <br />
-        <label htmlFor="lastName">Last Name: </label>
-        <input type="text" id="lastName" placeholder="Last Name" /> <br />
-        <label htmlFor="email">Email: </label>
-        <input type="email" id="email" placeholder="Example@email.com" /> <br />
-        <label htmlFor="password">Password: </label>
-        <input type="password" id="password" placeholder="Password" /> <br />
-        <input type="submit"/> 
-      </form>
+    <Button>Test</Button>
 
     </div>
   );
---
+++ src/components/Buttons.js
@@ -0,0 +1,11 @@
+import style from "styled-components";
+
+const colorRed = 'red'
+const colorBlue = 'blue'
+const colorGreen = 'green'
+
+export const Button = style.button`
+    width: 200px;
+    height: 50px;
+    background-color: ${colorBlue};
+`;
