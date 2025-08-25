8980e2168638884b3590cdb2eee25e671d06c511
Amal A M <vectorhero007@gmail.com>
2025-08-25 09:58:57 +0530

    Axios Api Request

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
-// import { useEffect, useRef, useState } from "react";
+import axios from 'axios';
 import './App.scss'
-import { Button } from './components/Buttons';
+import { useEffect, useState } from 'react';
 
 
 function App() {
 
+  // const [url, setUrl] = useState('')
+  const [products, setProducts] = useState([])
+  const [error, setError] = useState(null);      // error handling
+  const [loading, setLoading] = useState(true);  // loading state
+
+
+// useEffect(() => { ... }, [dependencies])
+// It runs the function inside whenever the component renders.
+// But when it runs depends on the dependencies you put in that array.
+
+
+  useEffect(() => {
+    axios.get('https://api.escuelajs.co/api/v1/products')
+    .then((res) => {
+      setProducts(res.data)
+      setLoading(false);   // 👈 important!
+    })
+    .catch((err) => {
+      setError(err.message)
+      setLoading(false);
+      console.log(error);
+    })
+  }, []) // ✅ runs only once when component mounts
+
+  if (loading) return <h2>Loading...</h2>;
+  if (error) return <h2>Error: {error}</h2>;
+
 
   return (
     <div className="App">
+      
+      <ul>
+        {products.map((p) => (
+          <li key={p.id}>{p.title} - {p.price}</li>
+        ))}
+      </ul>
 
-    <Button color={"blue"} darkColor={"darkblue"} large>Large Button</Button>
-    <Button color={"green"} darkColor={"darkgreen"}>Small Button</Button>
 
     </div>
   );
