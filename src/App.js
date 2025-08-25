937f2cdf62fb73454fac83ee072dab71f0452bd6
Amal A M <vectorhero007@gmail.com>
2025-08-25 09:22:21 +0530

    forms in react

--- src/App.js
+++ src/App.js
@@ -1,50 +1,24 @@
-import { useEffect, useRef, useState } from "react";
+// import { useEffect, useRef, useState } from "react";
 import './App.scss'
-import Task from "./components/Task";
-
+// import Task from "./components/Task";
 
 function App() {
 
-  // const [taskName, setTaskName] = useState("")
-  // const [time, setTime] = useState("")
-  const [taskList, setTaskList] = useState([])
-
-  const inputName = useRef(null)
-  const inputTime = useRef(null)
-
-  const addTask = () => {
-    setTaskList([...taskList, {task: inputName.current.value, time: inputTime.current.value}])
-
-    // This will log the old value (because update hasn't applied yet)
-    console.log("Immediately after set:", taskList); 
-    
-  }
-
-  useEffect(() => {
-    console.log("updated list" + taskList);
-  }, [taskList]);
 
   return (
     <div className="App">
 
-      <div class="container">
-        <h1>Todo list</h1>
-
-        <label>Task Name: </label>
-        <input type="text" id="task" ref={inputName} />
-
-        <label>Task Time: </label>
-        <input type="text" id="task" ref={inputTime} />
-
-        <button onClick={addTask}>Add</button>
-
-      </div>
-
-      {/* <Task taskName="do homework" time="20 minutes" /> */}
-
-      {taskList.map((task) => {
-        return <Task taskName={task.task} time={task.time} />
-      })}
+      <form>
+        <label htmlFor="firstName">First Name: </label>
+        <input type="text" id="firstName" placeholder="First Name" /> <br />
+        <label htmlFor="lastName">Last Name: </label>
+        <input type="text" id="lastName" placeholder="Last Name" /> <br />
+        <label htmlFor="email">Email: </label>
+        <input type="email" id="email" placeholder="Example@email.com" /> <br />
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" placeholder="Password" /> <br />
+        <input type="submit"/> 
+      </form>
 
     </div>
   );
